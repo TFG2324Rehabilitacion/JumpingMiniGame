8ac6bb4caa860e1226c2f9dc4591de769470f853
Juan Romo <72462333+JuanRomo-dev@users.noreply.github.com>
2024-04-09 17:16:21 +0200

    juego

---
+++ Assets/Scripts/PlataformGenerator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject plataforma;
+    public int numPlataformas = 10;
+
+    void Start()
+    {
+        GenerarPlataformas();
+    }
+
+    void GenerarPlataformas()
+    {
+        float plataformaH = plataforma.GetComponent<SpriteRenderer>().bounds.size.y;
+        Vector3 posicionInicial = transform.position;
+        for (int i = 0; i < numPlataformas; i++)
+        {
+            Vector3 nuevaPos = posicionInicial + new Vector3(0, i * plataformaH, -5);
+            Instantiate(plataforma, nuevaPos, Quaternion.identity);
+        }
+    }
+    // Update is called once per frame
+}
